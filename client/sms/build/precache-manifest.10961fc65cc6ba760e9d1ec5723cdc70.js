72624a194ca70974887e21491fe91a9d7ff15b0b
afreena.anjum <anjumafreena@gmail.com>
2019-08-31 18:05:58 +0530

    Deployment

---
+++ client/sms/build/precache-manifest.10961fc65cc6ba760e9d1ec5723cdc70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "581ff54c222b8e93ada413c36d773b54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1757f310f3fdc9ce686f",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "74ba2d611737098e0668",
+    "url": "/static/css/main.47dbd607.chunk.css"
+  },
+  {
+    "revision": "1757f310f3fdc9ce686f",
+    "url": "/static/js/2.0444968c.chunk.js"
+  },
+  {
+    "revision": "74ba2d611737098e0668",
+    "url": "/static/js/main.cc51bd30.chunk.js"
+  },
+  {
+    "revision": "6b4cde28b98a8c528edb",
+    "url": "/static/js/runtime~main.959d9e7e.js"
+  }
+]);
